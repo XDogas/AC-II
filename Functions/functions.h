6b446047a3eb332abf919c99a0d0df5d03755a41
XDogas <jonhydogas@gmail.com>
2020-04-29 17:59:15 +0100

    Functions

---
+++ Functions/functions.h
@@ -0,0 +1,7 @@
+#include "toBcd.c"
+#include "send2displays.c"
+#include "comDrv_config.c"
+
+unsigned char toBcd(unsigned char);
+void send2displays(unsigned char);
+void comDrv_config(unsigned int, char, unsigned int);
